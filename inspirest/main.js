c0b6d587ed8286e158cd3518db851d573fa02139
IsabelMolero <isabelmolerocasado@gmail.com>
2025-07-24 18:09:39 +0200

    Creación proyecto - apiUnsplash

---
+++ inspirest/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+import { createApi } from 'unsplash-js'
+
+const unsplash = createApi({
+  accessKey: import.meta.env.VITE_ACCESS_KEY
+})
+
+const searchPhotos = async (keyword) => {
+  const images = await unsplash.search.getPhotos({
+    query: keyword,
+    page: 1,
+    perPage: 30
+  })
+  return images
+}
+
+const solicitud = await searchPhotos('cat')
+
+console.log(solicitud)
